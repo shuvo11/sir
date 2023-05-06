cea53e3e9de7147a2c95cd167a4796b7dc75dc2c
Arup <arup@whyazure.onmicrosoft.com>
2023-05-06 17:27:57 +0600

    update

--- asset/js/main.js
+++ asset/js/main.js
@@ -3,3 +3,21 @@ $(document).ready(function(){
       $(".web-pro").toggle();
     });
   });
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
